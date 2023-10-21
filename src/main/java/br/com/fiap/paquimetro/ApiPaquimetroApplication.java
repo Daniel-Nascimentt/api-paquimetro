283781ac3fc7b5c0da10a600d68ca1367cbb2e4b
Daniel-Nasciment <daniel.fecaf@outlook.com>
2023-10-21 14:08:10 -0300

    Setup Inicial do projeto

---
+++ src/main/java/br/com/fiap/paquimetro/ApiPaquimetroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.paquimetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPaquimetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPaquimetroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/paquimetro/ApiPaquimetroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.paquimetro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPaquimetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
