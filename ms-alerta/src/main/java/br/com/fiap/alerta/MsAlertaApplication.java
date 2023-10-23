fbe6c6495a2359d16c84e25968dafde8be68d2af
Daniel-Nasciment <daniel.fecaf@outlook.com>
2023-10-23 15:35:22 -0300

    Setuo inicial demais micro services

---
+++ ms-alerta/src/main/java/br/com/fiap/alerta/MsAlertaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.alerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAlertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAlertaApplication.class, args);
+	}
+
+}
---
+++ ms-recibo/src/main/java/br/com/fiap/recibo/MsReciboApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.recibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReciboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReciboApplication.class, args);
+	}
+
+}
